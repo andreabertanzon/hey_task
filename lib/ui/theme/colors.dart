7446af85c6b03d44a33a770c1534619477596c3e
andreabertanzon <andrea.bertanzon@protonmail.com>
2022-02-19 16:14:43 +0100

    added static color file

---
+++ lib/ui/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LightColors{
+  static const backgroundColor = Color.fromARGB(255, 247, 248, 253);
+  static const defaultContainerColor = Color.fromARGB(255, 254, 254, 254);
+  static const iconsGrey = Color.fromARGB(255, 130, 138, 175);
+  static const subTitleGrey = Color.fromARGB(255, 160, 166, 189);
+  static const iconBlue = Color.fromARGB(255, 12, 89, 252);
+  static const drawerBlue = Color.fromARGB(255, 11, 22, 75);
+  static const defaultTaskColor = Color.fromARGB(255, 81, 119, 191);
+}
